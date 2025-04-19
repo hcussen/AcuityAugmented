f80878359501020b5875a3d9bc00aadf9b7f022f
Hannah Cussen <hcussen6@gmail.com>
2025-04-18 23:46:14 -0600

    make initial migration

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from sqlalchemy import ForeignKey, String, Datetime
+from sqlalchemy import String, DateTime, Integer, Boolean
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -7,11 +7,11 @@ class Base(DeclarativeBase):
 
 class Appointment(Base):
     __tablename__ = "appointments"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
-    created_at: Mapped[str] = mapped_column(Datetime)
-    deleted_at: Mapped[str] = mapped_column(Datetime)
-    is_deleted: Mapped[bool] = mapped_column(int)
+    created_at: Mapped[str] = mapped_column(DateTime)
+    deleted_at: Mapped[str] = mapped_column(DateTime)
+    is_deleted: Mapped[bool] = mapped_column(Boolean)
 
     def __repr__(self) -> str:
         return f"Appt #{self.id}: {self.name} created_at: {self.created_at} deleted: {self.is_deleted} at {self.deleted_at}"
