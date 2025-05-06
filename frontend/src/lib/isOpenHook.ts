8eb526bebbf352f64a2cabf660b9b6792eb1ac51
Hannah Cussen <hcussen6@gmail.com>
2025-05-06 10:54:49 -0400

    add hours config

--- frontend/src/lib/isOpenHook.ts
+++ frontend/src/lib/isOpenHook.ts
@@ -1,9 +1,20 @@
+import { openingHours } from "./settings"
+
 /**
  * Use this function to return if the center is currently open
  */
+
+const snapshotTime = Object.entries(openingHours).map(([key, value]) => {
+  // todo: finish the timedelta of value[0] - 30 min
+  return { key: value[0] }
+})
 
 export function wasSnapshotTaken(): boolean {
   // look at settings timing config
   // return true if the current time is after open time - 30 min
   return false
+}
+
+export function todaySnapshotTime(): string {
+  return ""
 }
---
+++ frontend/src/lib/settings.ts
@@ -0,0 +1,16 @@
+/**
+ * shows hours that the center is open.
+ * key: integer representing day of week, monday = 0
+ * openingHours[day][0] = open
+ * openingHours[day][1] = close
+ */
+
+export const openingHours = {
+  0: ["16:00", "20:00"],
+  1: ["16:00", "20:00"],
+  2: ["16:00", "20:00"],
+  3: ["16:00", "20:00"],
+  4: ["16:00", "19:00"],
+  5: ["10:00", "13:00"],
+  6: ["00:00", "00:00"],
+}
